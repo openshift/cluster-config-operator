3dcbd7b8a46fc72a1853ceb30f356d713fae7c9f
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2020-04-20 15:55:30 -0700

    add migration_aws_status controller
    This controller is responsible for upgrading existing `infrastructure.config.openshift.io/v1` `cluster` object
    for AWS to include the `AWSPlatformStatus` based on the InstallConfig stored in the ConfigMap `kube-system/cluster-config-v1`.
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1814332

---
+++ pkg/operator/migration_aws_status/controller_test.go
@@ -0,0 +1,204 @@
+package migration_aws_status
+
+import (
+	"context"
+	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	configfakeclient "github.com/openshift/client-go/config/clientset/versioned/fake"
+	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
+	"github.com/openshift/library-go/pkg/controller/factory"
+	"github.com/openshift/library-go/pkg/operator/events"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	ktesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/cache"
+)
+
+func Test_sync(t *testing.T) {
+	cases := []struct {
+		inputstatus configv1.InfrastructureStatus
+		inputdata   map[string]string
+
+		outputstatus configv1.InfrastructureStatus
+		err          string
+		actions      int
+	}{{
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType},
+		actions:      0,
+	}, {
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.PlatformType("random")},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.PlatformType("random")},
+		actions:      0,
+	}, {
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AzurePlatformType}},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AzurePlatformType}},
+		actions:      0,
+	}, {
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AzurePlatformType, Azure: &configv1.AzurePlatformStatus{ResourceGroupName: "test-rg"}}},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AzurePlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AzurePlatformType, Azure: &configv1.AzurePlatformStatus{ResourceGroupName: "test-rg"}}},
+		actions:      0,
+	}, {
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType, AWS: &configv1.AWSPlatformStatus{Region: "test-region"}}},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType, AWS: &configv1.AWSPlatformStatus{Region: "test-region"}}},
+		actions:      0,
+	}, {
+		inputstatus:  configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType, AWS: &configv1.AWSPlatformStatus{Region: "test-region", ServiceEndpoints: []configv1.AWSServiceEndpoint{{Name: "ec2", URL: "https://ec2.local"}}}}},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType, AWS: &configv1.AWSPlatformStatus{Region: "test-region", ServiceEndpoints: []configv1.AWSServiceEndpoint{{Name: "ec2", URL: "https://ec2.local"}}}}},
+		actions:      0,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType}},
+		err:         `^install-config key doesn't exist in ConfigMap kube-system/cluster-config-v1$`,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType},
+		err:         `^install-config key doesn't exist in ConfigMap kube-system/cluster-config-v1$`,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType},
+		inputdata:   map[string]string{"random-key": "random-value"},
+		err:         `^install-config key doesn't exist in ConfigMap kube-system/cluster-config-v1$`,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType},
+		inputdata: map[string]string{
+			"install-config": `apiVersion: v1
+baseDomain: testing.openshift.com
+compute:
+- hyperthreading: Enabled
+  name: worker
+  replicas: 3
+controlPlane:
+  hyperthreading: Enabled
+  name: master
+  replicas: 3
+metadata:
+  name: testing
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  machineCIDR: 10.0.0.0/16
+  networkType: OpenShiftSDN
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  azure:
+    baseDomainResourceGroupName: os4-common
+    region: centralus
+pullSecret: REDACTED
+sshKey: REDACTED`,
+		},
+		err: `^no AWS configuration found in cluster-config-v1$`,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType},
+		inputdata: map[string]string{
+			"install-config": `{
+  "apiVersion": "v1",
+  "baseDomain": "testing.openshift.com",
+  "compute": [
+    {
+      "hyperthreading": "Enabled",
+      "name": "worker",
+      "replicas": 3
+    }
+  ],
+  "controlPlane": {
+    "hyperthreading": "Enabled",
+    "name": "master",
+    "replicas": 3
+  },
+  "metadata": {
+    "name": "testing"
+  },
+  "networking": {
+    "clusterNetwork": [
+      {
+        "cidr": "10.128.0.0/14",
+        "hostPrefix": 23
+      }
+    ],
+    "machineCIDR": "10.0.0.0/16",
+    "networkType": "OpenShiftSDN",
+    "serviceNetwork": [
+      "172.30.0.0/16"
+    ]
+  },
+  "platform": {
+    "azure": {
+      "baseDomainResourceGroupName": "os4-common",
+      "region": "centralus"
+    }
+  },
+  "pullSecret": "REDACTED",
+  "sshKey": "REDACTED"
+}`,
+		},
+		err: `^no AWS configuration found in cluster-config-v1$`,
+	}, {
+		inputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType},
+		inputdata: map[string]string{
+			"install-config": `apiVersion: v1
+baseDomain: testing.openshift.com
+compute:
+- hyperthreading: Enabled
+  name: worker
+  replicas: 3
+controlPlane:
+  hyperthreading: Enabled
+  name: master
+  replicas: 3
+metadata:
+  name: testing
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  machineCIDR: 10.0.0.0/16
+  networkType: OpenShiftSDN
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  aws:
+    region: testing-region
+pullSecret: REDACTED
+sshKey: REDACTED`,
+		},
+		outputstatus: configv1.InfrastructureStatus{Platform: configv1.AWSPlatformType, PlatformStatus: &configv1.PlatformStatus{Type: configv1.AWSPlatformType, AWS: &configv1.AWSPlatformStatus{Region: "testing-region"}}},
+		actions:      1,
+	}}
+	for _, test := range cases {
+		t.Run("", func(t *testing.T) {
+			infra := &configv1.Infrastructure{ObjectMeta: metav1.ObjectMeta{Name: "cluster"}, Status: test.inputstatus}
+			indexerInfra := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+			if err := indexerInfra.Add(infra); err != nil {
+				t.Fatal(err.Error())
+			}
+			fakeConfig := configfakeclient.NewSimpleClientset(infra)
+
+			cm := &corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "cluster-config-v1", Namespace: "kube-system"}, Data: test.inputdata}
+			fake := fake.NewSimpleClientset(cm)
+
+			ctrl := MigrationAWSStatusController{
+				infraClient:     fakeConfig.ConfigV1().Infrastructures(),
+				infraLister:     configv1listers.NewInfrastructureLister(indexerInfra),
+				configMapClient: fake.CoreV1(),
+			}
+
+			err := ctrl.sync(context.TODO(),
+				factory.NewSyncContext("MigrationAWSStatusController", events.NewInMemoryRecorder("MigrationAWSStatusController")))
+			if test.err == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, test.actions, len(fakeConfig.Actions()))
+				got := infra.DeepCopy()
+				for _, a := range fakeConfig.Actions() {
+					obj := a.(ktesting.UpdateAction).GetObject().(*configv1.Infrastructure)
+					got.Status = obj.Status
+				}
+				assert.EqualValues(t, test.outputstatus, got.Status)
+			} else {
+				assert.Regexp(t, test.err, err.Error())
+			}
+		})
+	}
+}
