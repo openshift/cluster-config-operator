58c4a37408fc9aefbc4ed653d8469ab76a90352b
W. Trevor King <wking@tremily.us>
2020-12-07 15:15:00 -0800

    pkg/operator/kube_cloud_config: Fix "kuberneted" -> "Kubernetes" doc typo
    The typo is from a854f43ce1 (operator: add kube_cloud_config
    controller, 2020-04-08, #120).

--- pkg/operator/kube_cloud_config/doc.go
+++ pkg/operator/kube_cloud_config/doc.go
@@ -1,3 +1,3 @@
-// Package kubecloudconfig controller is responsible for stitching the user provided kuberneted cloud configuration file and
+// Package kubecloudconfig controller is responsible for stitching the user-provided Kubernetes cloud configuration file and
 // the various platform specific settings provided in the infrastructures.config.openshift.io
 package kubecloudconfig
