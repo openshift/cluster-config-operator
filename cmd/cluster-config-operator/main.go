aca363fb123a0fcca2ec2e640b0875fd2e570abf
ehila <ehila@redhat.com>
2022-01-10 09:32:54 -0500

    feat: updated main to use component-base/cli
    updated to use simplified component-base/cli pkg
    componenet-base/cli also addresses appending logging flags to command line
    
    Signed-off-by: ehila <ehila@redhat.com>

--- cmd/cluster-config-operator/main.go
+++ cmd/cluster-config-operator/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	goflag "flag"
-	"fmt"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 
-	utilflag "k8s.io/component-base/cli/flag"
-	"k8s.io/component-base/logs"
+	"k8s.io/component-base/cli"
 
 	"github.com/openshift/cluster-config-operator/pkg/cmd/operator"
 	"github.com/openshift/cluster-config-operator/pkg/cmd/render"
@@ -19,19 +13,9 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
-	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
-
-	logs.InitLogs()
-	defer logs.FlushLogs()
-
 	command := NewOperatorCommand()
-	if err := command.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
+	code := cli.Run(command)
+	os.Exit(code)
 }
 
 func NewOperatorCommand() *cobra.Command {
